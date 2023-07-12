ac25180cc88820ddba5b9bdf6e244eb20de93ec1
Lee-hye-won <hewon112311@gmail.com>
2023-07-12 16:12:15 +0900

    up

---
+++ src/main/java/com/shopmax/dto/ItemRankDto.java
@@ -0,0 +1,11 @@
+package com.shopmax.dto;
+
+public interface ItemRankDto {
+
+	int getNum();
+	long getId();
+	String getItemNm();
+	int getPrice();
+	String getImgUrl();
+	String getRepImgYn();
+}
--- src/main/java/com/shopmax/service/ItemService.java
+++ src/main/java/com/shopmax/service/ItemService.java
@@ -1,17 +1,26 @@
 package com.shopmax.service;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.shopmax.dto.ItemFormDto;
+import com.shopmax.dto.ItemImgDto;
+import com.shopmax.dto.ItemRankDto;
+import com.shopmax.dto.ItemSearchDto;
+import com.shopmax.dto.MainItemDto;
 import com.shopmax.entity.Item;
 import com.shopmax.entity.ItemImg;
 import com.shopmax.repository.ItemImgRepository;
 import com.shopmax.repository.ItemRepository;
 
-import jakarta.transaction.Transactional;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -49,6 +58,74 @@ public class ItemService {
 		}
 		
 		return item.getId();
+	}
+	
+	// 등록한 상품데이터 가져오기
+	@Transactional(readOnly = true)	// 트랜잭션 읽기 전용(변경감지를 수행하지않음) -> 성능향상
+	public ItemFormDto getItemDtl(Long itemId) {
+		// 1.item_img 테이블의 이미지를 가져온다.
+		List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+		
+		// ItemImg 엔티티 -> ItemImgDto로 변환 (view단에서 사용하는 것은 dto이고 컨트롤러에게 보낼때는 dto를 이용해야하기 때문)
+		List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+		for(ItemImg itemImg : itemImgList) {
+			ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+			itemImgDtoList.add(itemImgDto);
+		}
+		
+		// 2.item 테이블에 있는 데이터를 가져온다.
+		Item item = itemRepository.findById(itemId)
+								  .orElseThrow(EntityNotFoundException::new);
+		
+		// Item entity 객체 -> dto로 변환
+		ItemFormDto itemFormDto = ItemFormDto.of(item);
+		
+		// 3.ItemFormDto에 이미지정보 (itemImgDtiList)를 넣어준다.
+		itemFormDto.setItemImgDtoList(itemImgDtoList);
+		
+		return itemFormDto;
+		
+	}
+	
+	// 상품 수정하기(update)
+	public Long updateItem(ItemFormDto itemFormDto,
+			List<MultipartFile> itemImgFileList) throws Exception {
+		
+		// item 엔티티 가져외서 내용 바꾸기
+		Item item = itemRepository.findById(itemFormDto.getId())
+								  .orElseThrow(EntityNotFoundException::new);
+		
+		//update쿼리문 실행
+				/* ★★★ 한번 insert를 진행하면 엔티티가 영속성 컨텍스트에 저장이 되므로 
+				그 이후에는 변경감지 기능이 동작하기 때문에 개발자는 update쿼리문을 쓰지 않고
+				엔티티 데이터만 변경해주면 된다. */
+				item.updateItem(itemFormDto);
+				
+				//2. item_img를 바꿔준다. -> 5개의 레코드 전부 변경
+				List<Long> itemImgIds = itemFormDto.getItemImgIds(); //상품 이미지 아이디 리스트 조회
+				
+				for(int i=0; i<itemImgFileList.size(); i++) {
+					itemImgService.updateItemImg(itemImgIds.get(i), itemImgFileList.get(i));
+				}
+				
+				return item.getId(); //변경한 item의 id 리턴
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable) {
+		Page<Item> itemPage = itemRepository.getAdminItemPage(itemSearchDto, pageable);
+		return itemPage;
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<MainItemDto>  getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable) {
+		Page<MainItemDto> mainItemPage = itemRepository.getMainItemPage(itemSearchDto, pageable);
+		return mainItemPage;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<ItemRankDto> getItemRankList() {
+		return itemRepository.getItemRankList();
 	}
 }
 
